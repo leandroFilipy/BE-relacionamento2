48f3e6a97d85004c33ae812f2216c14e07f084d5
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-04-08 14:27:41 -0300

    feat: implementando o response entity

--- src/main/java/com/relacionamento/atv2/controller/ClientController.java
+++ src/main/java/com/relacionamento/atv2/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.relacionamento.atv2.dto.client.ClientRequest;
 import com.relacionamento.atv2.dto.client.ClientResponse;
 import com.relacionamento.atv2.service.ClienteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,28 +19,29 @@ public class ClientController {
     private final ClienteService clienteService;
 
     @PostMapping
-    public ClientResponse create(@RequestBody ClientRequest clientRequest){
-        return clienteService.create(clientRequest);
+    public ResponseEntity<ClientResponse> create(@RequestBody ClientRequest clientRequest){
+        return ResponseEntity.status(201).body(clienteService.create(clientRequest));
     }
 
     @GetMapping
-    public List<ClientResponse> list(){
-        return clienteService.list();
+    public ResponseEntity<List<ClientResponse>> list(){
+        return ResponseEntity.status(200).body(clienteService.list());
     }
 
     @GetMapping("/{id}")
-    public ClientResponse listById(@PathVariable long id){
-        return clienteService.listById(id);
+    public ResponseEntity<ClientResponse> listById(@PathVariable long id){
+        return ResponseEntity.status(200).body(clienteService.listById(id));
     }
 
     @PutMapping("/{id}")
-    public ClientResponse update(@PathVariable long id, @RequestBody ClientRequest clientRequest){
-        return clienteService.update(id,clientRequest);
+    public ResponseEntity<ClientResponse> update(@PathVariable long id, @RequestBody ClientRequest clientRequest){
+        return ResponseEntity.status(200).body(clienteService.update(id,clientRequest));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id){
+    public ResponseEntity<Void> delete(@PathVariable long id){
         clienteService.delete(id);
+       return ResponseEntity.status(204).build();
     }
 
 }
